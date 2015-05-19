a0e5800f6ac493e448f6516e4d6ced2a523f227b
junimpcsd <junimpcsd@live.com>
2015-05-19 18:02:38 -0300

    Arquivo da pilha onde ficam todos os métodos necessários para a execução do programa, porém, sem método principal.

---
+++ PilhaEncadeada_Junim/src/pilhaencadeada_junim/PilhaE.java
@@ -0,0 +1,73 @@
+package pilhaencadeada_junim;
+
+public class PilhaE implements interfacePilhaEncadeada{
+
+    private int tamanho = 0;
+    private NoE topo = null;
+    
+    @Override
+    public void criarPilha() {
+        tamanho = 0;
+        topo = null;
+        System.out.println("Pilha criada!");
+    }
+
+    @Override
+    public boolean eVazia() {
+        return tamanho == 0 || topo == null;
+    }
+
+    @Override
+    public int tamanho() {
+        return tamanho;
+    }
+
+    @Override
+    public void limpar() {
+        tamanho = 0;
+        topo = null;
+    }
+
+    @Override
+    public NoE push(int valor) {
+            tamanho = tamanho + 1;
+            NoE aux = new NoE(valor, topo);
+            topo = aux;
+            System.out.println("Elemento "+valor+" adicionado.");
+            return aux;
+    }
+
+    @Override
+    public NoE pop() {
+        if (eVazia()) {
+            System.out.println("Pilha vazia, não posso retirar o elemento desejado.");
+            return null;
+        } else {
+            tamanho = tamanho - 1;
+            NoE s = topo;
+            System.out.println("Elemento "+s.getNumero()+" retirado.");
+            topo = s.getProximo();
+            return s;
+        }
+    }
+
+    @Override
+    public int pegarTopo() {
+        return topo.getNumero();
+    }
+
+    @Override
+    public void listar() {
+        NoE aux;
+        if(topo == null){
+            System.out.println("A pilha está vazia!");
+        } else {
+            aux = topo;
+            while(aux!=null){
+                System.out.println(aux.getNumero());
+                aux = aux.getProximo();
+            }
+        }
+    }
+    
+}
