d34a9db5ec91ec46d8f733102902a7a9a0828733
junimpcsd <junimpcsd@live.com>
2015-05-26 03:46:28 -0300

    Nó da Fila Circular.

---
+++ Fila/src/filaCircular/NoFC.java
@@ -0,0 +1,17 @@
+package filaCircular;
+public class NoFC {
+    private int valor;
+
+    public NoFC(int valor) {
+        this.valor = valor;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+    
+}
