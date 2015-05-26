f0a38e3284eef231fa43433f222a5f0855cc68f6
junimpcsd <junimpcsd@live.com>
2015-05-26 02:40:38 -0300

    Nó da fila de arranjo simples.

---
+++ Fila/src/filaArranjoSimples/NoVS.java
@@ -0,0 +1,17 @@
+package filaArranjoSimples;
+public class NoVS {
+    private int valor;
+
+    public NoVS(int valor) {
+        this.valor = valor;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+    
+}
