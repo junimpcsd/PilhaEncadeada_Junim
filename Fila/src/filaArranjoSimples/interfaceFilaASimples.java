096cf94c017ebf5e491c65012ba42ab69b687599
junimpcsd <junimpcsd@live.com>
2015-05-26 02:40:53 -0300

    Interface da Fila de Arranjo Simples.

---
+++ Fila/src/filaArranjoSimples/interfaceFilaASimples.java
@@ -0,0 +1,12 @@
+package filaArranjoSimples;
+
+public interface interfaceFilaASimples {
+    void criarFila(int tamanho); //cria fila com tamanho    
+    boolean eCheia();  //verifica se a fila estar cheia     
+    boolean eVazia();  //verifica se a fila possui elementos     
+    int tamanho();  //retorna o número de itens da fila      
+    NoVS enfileirar(int valor);  //insere um valor no fim da fila     
+    NoVS desenfileirar();  //retira o nó do início da fila     
+    int pegarInicio();  //retorna o valor atual do início da fila     
+    void listar();  //retorna os valores da fila  
+}
