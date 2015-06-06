ad3576ffcdc0ed3d614a4964274812acd737b2e5
junimpcsd <junimpcsd@live.com>
2015-06-06 01:44:46 -0300

    Nó da árvore binária.

---
+++ ArvoreBinaria/src/arvorebinaria/NoArvore.java
@@ -0,0 +1,38 @@
+package arvorebinaria;
+public class NoArvore {
+    private int chave;
+    private NoArvore esquerda;
+    private NoArvore direita;
+
+    public NoArvore(int chave) {
+        this.chave = chave  ;
+        this.esquerda = null;
+        this.direita = null;
+    }
+
+    public int getChave() {
+        return chave;
+    }
+
+    public void setChave(int chave) {
+        this.chave = chave;
+    }
+
+    public NoArvore getEsquerda() {
+        return esquerda;
+    }
+
+    public void setEsquerda(NoArvore esquerda) {
+        this.esquerda = esquerda;
+    }
+
+    public NoArvore getDireita() {
+        return direita;
+    }
+
+    public void setDireita(NoArvore direita) {
+        this.direita = direita;
+    }
+    
+    
+}
