66299af6fbc6097e36878840a35ee51a6e964a6d
junimpcsd <junimpcsd@live.com>
2015-06-06 22:11:45 -0300

    Implementando métodos, falta o método de Inserir e Remover recursivamente, que eu não faço a mínima ideia de como é isso.

--- ArvoreBinaria/src/arvorebinaria/ArvoreBin.java
+++ ArvoreBinaria/src/arvorebinaria/ArvoreBin.java
@@ -16,21 +16,61 @@ public class ArvoreBin implements interfaceArvoreBinaria{
     @Override
     public NoArvore raiz() {
        raiz = new NoArvore(15);  
+       
+       return raiz;
     }
 
     @Override
     public NoArvore buscar(int valor) {
+        NoArvore inter = raiz;
         if (valor != 0) {  
-            while (valor.getEsquerda != 0) {  
-                node = node.esquerda;  
+            while (Integer.parseInt(inter.getEsquerda().toString()) != 0) {  
+                if(Integer.parseInt(inter.getEsquerda().toString()) < valor){
+                    inter = inter.getEsquerda();
+                }  else if(Integer.parseInt(inter.getEsquerda().toString()) == valor){
+                    inter = inter.getEsquerda();
+                    System.out.println("Valor encontrado!");
+                    return inter;
+                }
             }  
-        }  
-        return node;
-    }
+            while (Integer.parseInt(inter.getDireita().toString()) != 0) {
+                if(Integer.parseInt(inter.getDireita().toString()) > valor){
+                    inter = inter.getEsquerda();
+                }  else if(Integer.parseInt(inter.getDireita().toString()) == valor){
+                    inter = inter.getDireita();
+                    System.out.println("Valor encontrado!");
+                    return inter;
+                }
+            }
+            if(inter.getChave() == valor){
+                    return inter;
+            }
+        } else {
+            System.out.println("Valor inválido!");
+        }
+            return null;
+        } 
 
     @Override
     public void inserirItr(int valor) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        NoArvore aux = raiz;
+        if (aux != null){
+            if (valor < aux.getChave()){
+                if (aux.getEsquerda() != null) { 
+                   inserirItr(Integer.parseInt(aux.getEsquerda().toString())); 
+                } else { 
+                   aux.setEsquerda(new NoArvore(valor)); 
+                } 
+            } else if (valor > aux.getChave()){
+                if (aux.getDireita() != null) { 
+                   inserirItr(Integer.parseInt(aux.getDireita().toString())); 
+                } else { 
+                   aux.setDireita(new NoArvore(valor)); 
+                } 
+            }
+        } else {
+             raiz = new NoArvore(valor);
+        }
     }
 
     @Override
@@ -39,31 +79,48 @@ public class ArvoreBin implements interfaceArvoreBinaria{
     }
 
     @Override
-    public void removerItr(int valor, char tipo) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void removerItr(int valor) {
+        NoArvore aux = raiz;
+        if(aux == null){
+            System.out.println("Árvore vazia!");
+        } else {            
+            if(valor < aux.getChave()){
+                removerItr(Integer.parseInt(aux.getEsquerda().toString()));
+            } else if(valor > aux.getChave()){
+                removerItr(Integer.parseInt(aux.getDireita().toString()));
+            } else if (aux.getEsquerda() != null && aux.getDireita() != null) {
+                /*2 filhos*/  
+                System.out.println("Removido o nó: " + aux.getChave());
+                aux.setChave(Integer.parseInt(aux.getDireita().toString()));
+                aux.setDireita(aux.getDireita());
+            } else {  
+                System.out.println("Removido o nó: " + aux.getChave());  
+                aux = (aux.getEsquerda() != null) ? aux.getEsquerda() : aux.getDireita();  
+            }  
+        }
     }
 
     @Override
-    public void removerRec(int valor, char tipo) {
+    public void removerRec(int valor) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public void preOrdemItr(NoArvore no) {
        if(no != null){
-        System.out.print(no.getChave());
-         preOrdemItr(no.getEsquerda());
-         preOrdemItr(no.getDireita());
-         } 
+            System.out.print(no.getChave()+" ");
+            preOrdemItr(no.getEsquerda());
+            preOrdemItr(no.getDireita());
+        }
     }
 
     @Override
     public void posOrdemItr(NoArvore no) {
         if(no != null){
-          preOrdemItr(no.getEsquerda());
-          preOrdemItr(no.getDireita());
-         System.out.print(no.getChave());
-        } 
+            posOrdemItr(no.getEsquerda());
+            posOrdemItr(no.getDireita());
+            System.out.print(no.getChave()+" ");
+        }
     }
 
     @Override
